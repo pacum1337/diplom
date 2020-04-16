a2096291145e65ab7b17a1f72d54d6836bb5e9f9
Maxim Zagidulin <makcayka2000@gmail.com>
2020-04-16 16:29:06 +0300

    rucovoditeli (add, redact, delete) v1

--- UchetPractica/Rucovoditeli.Designer.cs
+++ UchetPractica/Rucovoditeli.Designer.cs
@@ -34,7 +34,8 @@
             this.bCancel = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,14 +43,8 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.cbSU = new System.Windows.Forms.ComboBox();
+            this.cbOrg = new System.Windows.Forms.ComboBox();
             this.tbName = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.LiderName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.LiderSurname = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Org = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.OrgName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -65,6 +60,7 @@
             this.button5.TabIndex = 22;
             this.button5.Text = "Добавить";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
@@ -77,6 +73,7 @@
             this.button4.TabIndex = 21;
             this.button4.Text = "Изменить";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -89,6 +86,7 @@
             this.button3.TabIndex = 20;
             this.button3.Text = "Удалить";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // bCancel
             // 
@@ -107,12 +105,6 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Id,
-            this.LiderName,
-            this.LiderSurname,
-            this.Org,
-            this.OrgName});
             this.dataGridView1.Location = new System.Drawing.Point(12, 11);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
@@ -124,7 +116,8 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label2);
@@ -132,31 +125,39 @@
             this.panel1.Controls.Add(this.button7);
             this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.textBox4);
-            this.panel1.Controls.Add(this.cbSU);
+            this.panel1.Controls.Add(this.cbOrg);
             this.panel1.Controls.Add(this.tbName);
-            this.panel1.Controls.Add(this.label5);
             this.panel1.Location = new System.Drawing.Point(699, 11);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(424, 304);
+            this.panel1.Size = new System.Drawing.Size(418, 356);
             this.panel1.TabIndex = 17;
+            this.panel1.Visible = false;
             // 
-            // comboBox1
+            // label3
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(184, 175);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(210, 30);
-            this.comboBox1.TabIndex = 38;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(15, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 24);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "Отчество";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(185, 175);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 28);
+            this.textBox1.TabIndex = 38;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(14, 178);
+            this.label6.Location = new System.Drawing.Point(15, 75);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(129, 24);
             this.label6.TabIndex = 37;
@@ -178,9 +179,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(15, 145);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 24);
+            this.label2.Size = new System.Drawing.Size(91, 24);
             this.label2.TabIndex = 34;
-            this.label2.Text = "Фимилия";
+            this.label2.Text = "Фамилия";
             // 
             // label1
             // 
@@ -196,25 +197,27 @@
             // 
             this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button7.Location = new System.Drawing.Point(274, 230);
+            this.button7.Location = new System.Drawing.Point(268, 290);
             this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(147, 55);
             this.button7.TabIndex = 31;
             this.button7.Text = "Подтвердить";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button6
             // 
             this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button6.Location = new System.Drawing.Point(3, 230);
+            this.button6.Location = new System.Drawing.Point(7, 290);
             this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(137, 55);
             this.button6.TabIndex = 30;
             this.button6.Text = "Отмена";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // textBox4
             // 
@@ -225,16 +228,16 @@
             this.textBox4.Size = new System.Drawing.Size(209, 28);
             this.textBox4.TabIndex = 29;
             // 
-            // cbSU
+            // cbOrg
             // 
-            this.cbSU.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbSU.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.cbSU.FormattingEnabled = true;
-            this.cbSU.Location = new System.Drawing.Point(185, 72);
-            this.cbSU.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.cbSU.Name = "cbSU";
-            this.cbSU.Size = new System.Drawing.Size(209, 30);
-            this.cbSU.TabIndex = 27;
+            this.cbOrg.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbOrg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbOrg.FormattingEnabled = true;
+            this.cbOrg.Location = new System.Drawing.Point(185, 72);
+            this.cbOrg.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbOrg.Name = "cbOrg";
+            this.cbOrg.Size = new System.Drawing.Size(209, 30);
+            this.cbOrg.TabIndex = 27;
             // 
             // tbName
             // 
@@ -245,62 +248,11 @@
             this.tbName.Size = new System.Drawing.Size(209, 28);
             this.tbName.TabIndex = 24;
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(15, 75);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(164, 24);
-            this.label5.TabIndex = 23;
-            this.label5.Text = "Руководитель от";
-            // 
-            // Id
-            // 
-            this.Id.HeaderText = "Id";
-            this.Id.MinimumWidth = 6;
-            this.Id.Name = "Id";
-            this.Id.ReadOnly = true;
-            this.Id.Visible = false;
-            this.Id.Width = 125;
-            // 
-            // LiderName
-            // 
-            this.LiderName.HeaderText = "Имя";
-            this.LiderName.MinimumWidth = 6;
-            this.LiderName.Name = "LiderName";
-            this.LiderName.ReadOnly = true;
-            this.LiderName.Width = 125;
-            // 
-            // LiderSurname
-            // 
-            this.LiderSurname.HeaderText = "Фамилия";
-            this.LiderSurname.MinimumWidth = 6;
-            this.LiderSurname.Name = "LiderSurname";
-            this.LiderSurname.ReadOnly = true;
-            this.LiderSurname.Width = 125;
-            // 
-            // Org
-            // 
-            this.Org.HeaderText = "Организация";
-            this.Org.MinimumWidth = 6;
-            this.Org.Name = "Org";
-            this.Org.ReadOnly = true;
-            this.Org.Width = 125;
-            // 
-            // OrgName
-            // 
-            this.OrgName.HeaderText = "Название организации";
-            this.OrgName.MinimumWidth = 6;
-            this.OrgName.Name = "OrgName";
-            this.OrgName.ReadOnly = true;
-            this.OrgName.Width = 125;
-            // 
             // Rucovoditeli
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1173, 434);
+            this.ClientSize = new System.Drawing.Size(1132, 434);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -310,6 +262,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Rucovoditeli";
             this.Text = "Руководители";
+            this.Load += new System.EventHandler(this.Rucovoditeli_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -325,7 +278,6 @@
         private System.Windows.Forms.Button bCancel;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
@@ -333,13 +285,9 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.ComboBox cbSU;
+        private System.Windows.Forms.ComboBox cbOrg;
         private System.Windows.Forms.TextBox tbName;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
-        private System.Windows.Forms.DataGridViewTextBoxColumn LiderName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn LiderSurname;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Org;
-        private System.Windows.Forms.DataGridViewTextBoxColumn OrgName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- UchetPractica/Rucovoditeli.cs
+++ UchetPractica/Rucovoditeli.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,243 @@ namespace UchetPractica
 {
     public partial class Rucovoditeli : Form
     {
+        private bool colRuc = false;
+        private bool addRed;
+        int[] orgId = new int[0];
+        int lenght = 0;
+        int selectRucId = -1;
         public Rucovoditeli()
         {
             InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            string sqlGroups = "SELECT R.Id, O.Name as OrgName, R.Name as RucName, " +
+                "R.Surname, R.Patronymic " +
+                "FROM Rucovoditeli AS R " +
+                "INNER JOIN Organizations AS O " +
+                "ON R.OrgId = O.Id";
+            
+            using (SqlConnection connect = new SqlConnection(Strings.ConStr))
+            {
+                connect.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlGroups, connect);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+
+                SqlCommand command = new SqlCommand(sqlGroups, connect);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    colRuc = true;
+                }
+                else
+                {
+                    colRuc = false;
+                }
+            }
+            dataGridView1.Columns[0].Visible = false;
+
+            dataGridView1.Columns[1].HeaderText = "Организация";
+            dataGridView1.Columns[2].HeaderText = "Имя";
+            dataGridView1.Columns[3].HeaderText = "Фамилия";
+            dataGridView1.Columns[4].HeaderText = "Отчество";
+
+            dataGridView1.Columns[1].Width = 160;
+        }
+
+        private void CBLoad()
+        {
+            cbOrg.Items.Clear();
+            string sqlSU = "SELECT Id, Name FROM Organizations";
+            using (SqlConnection connection = new SqlConnection(Strings.ConStr))
+            {
+                connection.Open();
+                SqlCommand sql = new SqlCommand(sqlSU, connection);
+                SqlDataReader reader = sql.ExecuteReader();
+                while (reader.Read())
+                {
+                    lenght++;
+                    Array.Resize(ref orgId, lenght);
+                    cbOrg.Items.Add(reader.GetString(1).ToString());
+                    orgId[lenght - 1] = reader.GetInt32(0);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void Rucovoditeli_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            addRed = true;
+            panel1.Visible = true;
+            CBLoad();
+            tbName.Text = "";
+            textBox4.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            addRed = false;
+            if (colRuc)
+            {
+                selectRucId = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+                if (selectRucId > 0)
+                {
+                    panel1.Visible = true;
+
+                    string orgName = Convert.ToString(dataGridView1.CurrentRow.Cells[1].Value);
+                    string name = Convert.ToString(dataGridView1.CurrentRow.Cells[2].Value);
+                    string surname = Convert.ToString(dataGridView1.CurrentRow.Cells[3].Value);
+                    string patr = Convert.ToString(dataGridView1.CurrentRow.Cells[3].Value);
+
+                    CBLoad();
+                    cbOrg.Text = orgName;
+                    tbName.Text = name;
+                    textBox4.Text = surname;
+                    textBox1.Text = patr;
+                }
+                else
+                {
+                    MessageBox.Show("Выберите группу для исправления!");
+                    panel1.Visible = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Нет записей для изменения");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (cbOrg.Text == "")
+            {
+                MessageBox.Show("Выберите организацию!");
+            }
+            else if (tbName.Text == "")
+            {
+                MessageBox.Show("Введите имя руководителя!");
+            }
+            else if (textBox4.Text == "")
+            {
+                MessageBox.Show("Введите фамилию руководителя!");
+            }
+            else
+            {
+                string id = orgId[cbOrg.SelectedIndex].ToString();
+                string name = tbName.Text.Trim();
+                string surname = textBox4.Text.Trim();
+                string patr = textBox1.Text.Trim();
+                if (addRed)//Добавление студента 
+                {
+                    bool haveSoStud = false;
+
+                    string sqlProv = String.Format("SELECT * FROM Rucovoditeli WHERE " +
+                    "OrgId='{0}' AND Name=N'{1}' AND Surname=N'{2}' AND Patronymic=N'{3}'"
+                    , id, name, surname, patr);
+
+                    using (SqlConnection connection = new SqlConnection(Strings.ConStr))
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand(sqlProv, connection);
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            MessageBox.Show("Этот руководитель уже зарегистрирован!");
+                            haveSoStud = true;
+                        }
+                    }
+
+                    if (!haveSoStud)
+                    {
+                        string sqlAddStud = String.Format("INSERT INTO Rucovoditeli " +
+                            "(OrgId, Name, Surname,Patronymic) " +
+                            "VALUES (N'{0}',N'{1}',N'{2}',N'{3}')"
+                            , id, name, surname, patr);
+
+                        using (SqlConnection connect = new SqlConnection(Strings.ConStr))
+                        {
+                            connect.Open();
+                            SqlCommand command = new SqlCommand(sqlAddStud, connect);
+                            int h = command.ExecuteNonQuery();
+                            if (h == 0) MessageBox.Show("Error!!");
+                            else MessageBox.Show("Добавлен новый руководитель!");
+                        }
+                        LoadData();
+                        panel1.Visible = false;
+                    }
+                }
+                else//Редактирование группы
+                {
+                    string sqlEditStud = String.Format("UPDATE Rucovoditeli SET OrgId = '{0}', " +
+                        "Name = N'{1}', Surname = N'{2}', Patronymic = N'{3}' " +
+                        "WHERE Id = '{4}'", id, name, surname, patr, selectRucId);
+                    using (SqlConnection connection = new SqlConnection(Strings.ConStr))
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand(sqlEditStud, connection);
+                        int h = command.ExecuteNonQuery();
+                        if (h > 0)
+                        {
+                            MessageBox.Show("Информация изменена");
+                        }
+                    }
+                    LoadData();
+                    panel1.Visible = false;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (colRuc)
+            {
+                selectRucId = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+                if (selectRucId > 0)
+                {
+                    DialogResult dr = MessageBox.Show("Вы уверены, что хотите удалить " +
+                        "руководителя из базы данных?", "Подтверждение действия",
+                        MessageBoxButtons.YesNo);
+                    if (dr == DialogResult.Yes)
+                    {
+                        string sqlDelStud = String.Format("DELETE FROM Rucovoditeli " +
+                                    "WHERE Id = '{0}'", selectRucId);
+
+                        using (SqlConnection connection = new SqlConnection(Strings.ConStr))//Удаление самой группы из бд
+                        {
+                            connection.Open();
+                            SqlCommand command = new SqlCommand(sqlDelStud, connection);
+                            int number = command.ExecuteNonQuery();
+                            if (number > 0)
+                            {
+                                MessageBox.Show("Руководитель удален из базы данных");
+                                LoadData();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Выберите руководителя для удаления!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Нет записей для удаления!");
+            }
+            panel1.Visible = false;
         }
     }
 }
